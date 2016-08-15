ffddfe70e0ca3b1ddda39877270b54fd2c5904bd
Eric Tsang <surplus.et@gmail.com>
2016-08-14 18:12:23 -0700

    added some more useful controls to the library
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

---
+++ src/main/kotlin/com/github/ericytsang/lib/javafxutils/ComplexComboBox.kt
@@ -0,0 +1,45 @@
+package com.github.ericytsang.lib.javafxutils
+
+import javafx.scene.Node
+import javafx.scene.control.ComboBox
+import javafx.scene.layout.VBox
+
+/**
+ * Created by surpl on 8/14/2016.
+ */
+class ComplexComboBox<Option:ComplexComboBox.OptionalBuilder<Product>,Product>:VBox()
+{
+    val comboBox = ComboBox<Option>().apply()
+    {
+        valueProperty().addListener()
+        {
+            observableValue,oldValue,newValue ->
+
+            if (oldValue.panel != null)
+            {
+                this@ComplexComboBox.children.remove(oldValue.panel)
+            }
+
+            if (newValue.panel != null)
+            {
+                this@ComplexComboBox.children.add(newValue.panel)
+            }
+        }
+    }
+
+    /**
+     * can be selected by the [comboBox].
+     */
+    interface OptionalBuilder<Product>
+    {
+        /**
+         * displayed in the [ComplexComboBox] control if selected by the
+         * [comboBox].
+         */
+        val panel:Node?
+
+        fun build():Product
+
+        fun configureBuilderFrom(product:Product)
+    }
+}
---
+++ src/main/kotlin/com/github/ericytsang/lib/javafxutils/ValidatableTextField.kt
@@ -0,0 +1,41 @@
+package com.github.ericytsang.lib.javafxutils
+
+import javafx.scene.control.TextField
+
+/**
+ * Created by surpl on 8/14/2016.
+ */
+abstract class ValidatableTextField:TextField()
+{
+    companion object
+    {
+        fun makeNumericTextField():TextField
+        {
+            return object:ValidatableTextField()
+            {
+                override fun validate(text:String):Boolean
+                {
+                    return text.matches(Regex("[0-9]*"))
+                }
+            }
+        }
+    }
+
+    override fun replaceText(start:Int,end:Int,text:String)
+    {
+        if (validate(text))
+        {
+            super.replaceText(start, end, text)
+        }
+    }
+
+    override fun replaceSelection(replacement:String)
+    {
+        if (validate(text))
+        {
+            super.replaceSelection(text)
+        }
+    }
+
+    protected abstract fun validate(text:String):Boolean
+}
